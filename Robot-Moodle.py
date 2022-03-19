4b2b3fe1aaa2c3c3e05ce2958aeaae8bc06a7d79
Mohammad <95560413+zakery1369@users.noreply.github.com>
2022-03-19 03:50:02 +0330

    Update Robot-Moodle.py

--- Robot-Moodle.py
+++ Robot-Moodle.py
@@ -1,4 +1,3 @@
-from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 import time
 
@@ -7,11 +6,10 @@ loginpassword = ['123456'];
 # Number of robots
 robot = 1
 
-counter = 0
-while counter < 410:
+while robot < 411:
     driver = webdriver.Firefox(executable_path="./geckodriver")
     # Moodle login address
-    driver.get("https://Example.com/login/index.php")
+    driver.get("https://zmoodle.ttop.ir/login/index.php")
 
     time.sleep(5)
     username =  driver.find_element_by_name("username")
@@ -19,15 +17,18 @@ while counter < 410:
     password =  driver.find_element_by_name("password")
     password.send_keys(loginpassword)
     login = driver.find_element_by_id("loginbtn").click()
+    
     # Word search to ensure correct login
     sourcetext=driver.page_source
     searchword="any word in this page"
     # True or False
     print(searchword in sourcetext)
+    
     # Show robot number 
-    print(str(robot))
+    print('Robot ' + str(robot))
 
     robot = robot +1
-    counter = counter + 1
     time.sleep(5)
+    
+    # Close browser
     driver.close()
